b8689b0b7670302ae985c3bf585e83353869b0a0
jackbud13 <jackbud@yandex.ru>
2019-10-17 17:17:05 +0300

    описание того, что вы сделали

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -3,7 +3,14 @@
  * @param {number} m base
  * @param {number} n index
  * @returns {number}
- */
+*/ 
+ 
 function pow(m, n) {
-
+  let x = 1;
+  for (let i = 1; i <= n; i++)
+  {
+    x = x * m;
+  }
+  return x;
 }
+
