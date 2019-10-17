af6e6f594be2595659c421659fd9b4b04cc7e532
jackbud13 <jackbud@yandex.ru>
2019-10-17 18:01:49 +0300

    pow, version 3

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -3,21 +3,16 @@
  * @param {number} m base
  * @param {number} n index
  * @returns {number}
-*/ 
-
-let x = prompt('Какое число возводим в степень?', 1),
-y = prompt('В какую степень возводим число?', 1);
-
-alert(pow(x,y));
-
- 
+*/
 function pow(m, n) {
   let result = 1;
-  for (let i = 1; i <= n; i++)
-  {
+  for (let i = 1; i <= n; i++) {
     result = result * m;
   }
   return result;
 }
 
+const x = prompt('Какое число возводим в степень?', 1);
+const y = prompt('В какую степень возводим число?', 1);
 
+alert(pow(x, y));
