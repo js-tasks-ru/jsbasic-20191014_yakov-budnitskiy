402171f0db44f57281440f692b2b6843c156b40c
jackbud13 <jackbud@yandex.ru>
2019-10-17 17:31:19 +0300

    функция возведения в степень с вызовом параметров

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -4,13 +4,20 @@
  * @param {number} n index
  * @returns {number}
 */ 
+
+let x = prompt('Какое число возводим в степень?', 1),
+y = prompt('В какую степень возводим число?', 1);
+
+alert(pow(x,y));
+
  
 function pow(m, n) {
-  let x = 1;
+  let result = 1;
   for (let i = 1; i <= n; i++)
   {
-    x = x * m;
+    result = result * m;
   }
-  return x;
+  return result;
 }
+
 
