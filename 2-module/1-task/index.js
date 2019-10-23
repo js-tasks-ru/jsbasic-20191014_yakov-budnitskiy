60e63263a02b8b432ae8c0cf3232c058c7353b02
jackbud13 <jackbud@yandex.ru>
2019-10-23 19:11:47 +0300

    module 2 task 1 ready

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -3,5 +3,19 @@
  * @param {Object} obj - клонируем объект
  * @returns {Object}
  */
-function clone(obj) {
+
+function clone(prime) {
+  const newObj = {};
+
+  for (const key in prime) {
+    if (prime[key] === null) {
+      newObj[key] = null;
+    } else if (typeof prime[key] === 'object') {
+      newObj[key] = clone(prime[key]);
+    } else {
+      newObj[key] = prime[key];
+    }
+  }
+
+  return newObj;
 }
