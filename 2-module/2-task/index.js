af719a24290439e2588e0ac7386f8fd0c9e2181c
jackbud13 <jackbud@yandex.ru>
2019-10-23 19:25:45 +0300

    nodule 2 task 2 ready

--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -4,4 +4,12 @@
  * @returns {Boolean}
  */
 function isEmpty(obj) {
+  let res = true;
+  for (const key in obj) {
+    if (obj[key] || obj[key] === null || obj[key] === undefined) {
+      res = false;
+      break;
+    }
+  }
+  return res;
 }
